8391c26f7f70f099b80f851622044f850141342e
Aneke Ifeanyi <anekeifeanyi999@gmail.com>
2022-09-16 22:29:40 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - checks for uppercase character
+ * main - prints Putchar
  *
- * Return: Always 0
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	char c;
-
-	c = 'A';
-	_putchar ("%A: %d\1", 'A', _isupper('A'));
-	c = 'a';
-	_putchar ("%a: %d\0", 'a', _isupper('a'));
+	putchar (");
+	-putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	-putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 	return (0);
 }
 
